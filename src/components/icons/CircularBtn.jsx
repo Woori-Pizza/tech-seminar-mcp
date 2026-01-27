b0c2d8a6cf92ca4f879a92dcd91eaa66dd685510
chaechaen <leece144@gmail.com>
2026-01-27 18:24:43 +0900

    [fix] add mouse hover handler props

--- src/components/icons/CircularBtn.jsx
+++ src/components/icons/CircularBtn.jsx
@@ -1,9 +1,11 @@
-export const CircularBtn = ({ icon, className = "", dataIdx }) => (
-    <button
-      type="button"
-      data-idx={dataIdx}
-      className={`h-9 w-9 rounded-full bg-black/30 text-white hover:bg-black/50 js-round-btn cursor-pointer ${className}`}
-    >
-      {icon}
-    </button>
-  );
+export const CircularBtn = ({ icon, className = "", dataIdx, onHover }) => (
+  <button
+    type="button"
+    data-idx={dataIdx}
+    className={`h-9 w-9 rounded-full bg-black/30 text-white hover:bg-black/50 js-round-btn cursor-pointer ${className}`}
+    onMouseEnter={() => onHover && onHover(true)}
+    onMouseLeave={() => onHover && onHover(false)}
+  >
+    {icon}
+  </button>
+);
